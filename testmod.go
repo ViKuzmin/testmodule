13b4ddd50ddaf9e336240dafa7144f22d6ba06f0
ViKuzmin <11v.kuzmin@gmail.com>
2024-05-09 11:21:07 +0300

    test1

--- testmod.go
+++ testmod.go
@@ -1,8 +1,15 @@
 package testmodule
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func SayBye() {
 	fmt.Println("ASDASDSAa!!!!")
 
+}
+
+func SaySomething() {
+	fmt.Println(rand.Int(), "asdsadd")
 }
